5df1106e9e0097ed4c80554c3d40c33d33c2086c
Autopilot-11 <leonhuang2029@u.northwestern.edu>
2026-07-01 11:14:39 +0800

    Add: is-palindrome - submission-8

---
+++ Data Structures & Algorithms/is-palindrome/submission-8.py
@@ -0,0 +1,30 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        ## 2 pointers
+        # left = 0
+        # right = len(s) - 1
+    
+        # while left <= right:
+        #     while not s[left].isalnum() and left < len(s)-1:
+        #         left += 1
+        #     while not s[right].isalnum() and right > 0:
+        #         right -= 1
+        #     if left > right:
+        #         return True 
+        #     if not s[left].lower() == s[right].lower():
+        #         return False
+        #     else:
+        #         left += 1
+        #         right -= 1
+        # return True
+
+        # String solution
+        newstr = ""
+        for c in s:
+            if c.isalnum():
+                newstr += c.lower()
+
+        return newstr == newstr[::-1]
+    
+
+        
